be4dd69b686ed61cf011b1dcbdbd4f3688366e69
guilosost <48485791+guilosost@users.noreply.github.com>
2019-05-09 19:14:48 +0200

    Update lista_ayuda.php

--- vista/listas/lista_ayuda.php
+++ vista/listas/lista_ayuda.php
@@ -34,9 +34,9 @@ $conexion = crearConexionBD();
 // , COMIDAS.BEBE, COMIDAS.NIÑO, AYUDASECONOMICAS.CANTIDAD, AYUDASECONOMICAS.PRIORIDAD, CURSOS.MATERIA, TRABAJOS.EMPRESA, TRABAJOS.SALARIOAPROXIMADO
 // , COMIDAS, AYUDASECONOMICAS, CURSOS, TRABAJOS
 // , PRIORIDAD, MATERIA, SALARIOAPROXIMADO
-$query = 'SELECT AYUDAS.CONCEDIDA, AYUDAS.SUMINISTRADAPOR, COMIDAS.BEBE, COMIDAS.NIÑO '
-    . 'FROM AYUDAS, COMIDAS '
-    . 'ORDER BY CONCEDIDA ASC';
+$query = 'SELECT USUARIOS.DNI, AYUDAS.CONCEDIDA, AYUDAS.SUMINISTRADAPOR, COMIDAS.BEBE, COMIDAS.NIÑO '
+    . 'FROM USUARIOS, AYUDAS, COMIDAS '
+    . 'ORDER BY DNI, CONCEDIDA ASC';
 
 // Se comprueba que el tamaño de página, página seleccionada y total de registros son conformes.
 // En caso de que no, se asume el tamaño de página propuesto, pero desde la página 1
@@ -134,14 +134,15 @@ cerrarConexionBD($conexion);
 
                         <div class="datos_ayuda">
 
+                        <input id="DNI" name="DNI" value="<?php echo $fila["DNI"]; ?>" />
 
                             <input id="CONCEDIDA" name="CONCEDIDA" value="<?php echo $fila["CONCEDIDA"]; ?>" />
 
                             <input id="SUMINISTRADAPOR" name="SUMINISTRADAPOR" value="<?php echo $fila["SUMINISTRADAPOR"]; ?>" />
 
-                            <input id="BEBE" name="BEBE" value="<?php echo $fila["BEBE"]; ?>" />
+                            <input id="BEBE" name="BEBE" type="hidden" value="<?php echo $fila["BEBE"]; ?>" />
 
-                            <input id="NIÑO" name="NIÑO" value="<?php echo $fila["NIÑO"]; ?>" />
+                            <input id="NIÑO" name="NIÑO" type="hidden" value="<?php echo $fila["NIÑO"]; ?>" />
 
                             <?php
 
